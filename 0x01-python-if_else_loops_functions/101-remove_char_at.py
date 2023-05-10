e09e8edfa1d743819e7efd6f242f1c7a3d195f10
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2023-05-10 01:31:36 +0000

    handled the negative cases

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     tmp = list(str)
-    if len(str) > n and n !< 0:
+    if len(str) > n and n >= 0:
         del tmp[n]
     new_str = "".join(tmp)
     return new_str
+
+print(remove_char_at("Chicago", -3))
