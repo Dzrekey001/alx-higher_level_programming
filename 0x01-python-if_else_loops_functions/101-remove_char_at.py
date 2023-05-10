9c9cc37fbec86ac2db4ed80c60d1be96154efb57
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2023-05-10 01:24:30 +0000

    added the negative case edge

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
     tmp = list(str)
-    if len(str) > n or n < 0:
+    if len(str) > n and n !< 0:
         del tmp[n]
     new_str = "".join(tmp)
     return new_str
