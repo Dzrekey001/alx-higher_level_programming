12c1a20b243de0cfdfb3c8ddcdf282d2a0073a31
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2023-05-12 02:18:00 +0000

    Updated 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ arg_len = len(argument)
 if arg_len == 1:
     print("{} arguments.".format(arg_len - 1))
 elif arg_len == 2:
-    print("{} argument.".format(arg_len - 1))
+    print("{} argument:".format(arg_len - 1))
 else:
     print("{} arguments:".format(arg_len - 1))
 for i in range(1, arg_len):
