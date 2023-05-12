08fec973ac8c3fdd651a12241599d7b9d30a28b2
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2023-05-12 03:34:43 +0000

    Updated 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import hidden_4
-
 if __name__ == "__main__":
-    names = sort(dir(hidden_4))
+    import hidden_4
+
+    names = dir(hidden_4)
     for name in names:
         if name[:2] != "__":
             print(name)
