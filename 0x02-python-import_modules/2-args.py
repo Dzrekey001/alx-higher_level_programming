67c303fb90a88f2f8d4d3a5ce7d4645b65c60ad7
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2023-05-12 02:26:20 +0000

    Updated 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 import sys
-argument = sys.argv
-arg_len = len(argument)
+arg_len = len(sys.argv)
 
 if arg_len == 1:
-    print("{} arguments.".format(arg_len - 1))
+    print("0 arguments.")
 elif arg_len == 2:
-    print("{} argument:".format(arg_len - 1))
+    print("1 argument:")
 else:
     print("{} arguments:".format(arg_len - 1))
 for i in range(1, arg_len):
-    print("{}: {}".format(i, argument[i]))
+    print("{}: {}".format(i, sys.argv[i]))
