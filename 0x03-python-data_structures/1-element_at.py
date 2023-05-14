afc86c40b50375cde46748304d3e0ae86756b18c
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2023-05-14 23:40:15 +0000

    pycodestyle correction

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,6 @@ def element_at(my_list, idx):
     if idx < 0:
         return (None)
     elif idx > (len(my_list) - 1):
-        return (None)
-    
+        return (None) 
     element = my_list[idx]
     return (element)
