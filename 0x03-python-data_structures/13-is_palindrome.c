cd66cd028679be59229528faaa4ed7ba12b651d7
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2023-05-16 05:35:27 +0000

    a function in C that checks if a singly linked list is a palindrome.

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,34 @@
+#include "list.h"
+/**
+ * is_palindrome - Checks if a singly linked list is a palindrome.
+ * @head: A pointer to the head of the linked list.
+ * Return: If the linked list is not a palindrome - 0. If the
+ * linked list is a palindrome - 1.
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *slow = *head;
+	listint_t *fast = *head;
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
+
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
+	while (fast != NULL && fast->next != NULL)
+	{
+		fast = fast->next->next;
+		next = slow->next;
+		slow->next = prev;
+		prev = slow;
+		slow = next;
+	}
+	if (fast != NULL)
+		slow = slow->next;
+	while (prev != NULL && slow != NULL)
+	{
+		if (prev->n != slow->n)
+			return (0);
+		prev = prev->next;
+		slow = slow->next;
+	}
+	return (1);
