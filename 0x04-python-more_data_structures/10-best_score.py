b1372bc816cb99b8fda1eafed1f2b9d9cc8933d1
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2023-05-18 04:05:27 +0000

    made corrections

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,4 @@
 def best_score(a_dictionary):
     if a_dictionary is None or a_dictionary == {}:
         return (None)
-    keys = list(a_dictionary.keys())
-    keys.sort()
-    return (keys[-1])
+    return (max(a_dictionary, key=a_dictionary.get))
