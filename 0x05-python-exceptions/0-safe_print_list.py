dcdeb065eb4f1f41082c4d81a4b53d17ef8ee68f
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2023-05-29 23:39:58 +0000

    made correction to use the .format to print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,10 +4,10 @@ def safe_print_list(my_list=[], x=0):
     list_len = 0
     try:
         for i in range(x):
-            list_len += 1
             print("{:d}".format(my_list[i]), end="")
+            list_len += 1
     except IndexError:
-        print(end="")
+        pass
     finally:
         print()
         return (list_len)
