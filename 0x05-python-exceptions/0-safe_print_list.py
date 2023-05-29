200cc5fae5aa8807dd44f2e45e3a3dc9eff1409f
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2023-05-29 23:13:07 +0000

    a function that prints x elements of a list safely

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    list_len = 0
+    try:
+        for element in my_list:
+            list_len += 1
+
+        for i in range(x):
+            print(my_list[i], end="")
+    except IndexError:
+        print(end="")
+    finally:
+        print()
+        return (list_len)
