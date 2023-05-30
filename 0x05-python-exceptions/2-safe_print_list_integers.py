c9067b8e4d480e8d56fe7884049a52a2990c59af
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2023-05-30 00:24:08 +0000

    made some indentation correction

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,5 +8,5 @@ def safe_print_list_integers(my_list=[], x=0):
             int_len += 1
         except (TypeError, ValueError):
             continue
-        print()
+    print()
     return (int_len)
