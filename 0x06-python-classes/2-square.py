8323b4dd53ab573e99f99b373cfe13832754623f
Daniel Kwasi Dzrekey <dkdzrekey001@gmail.com>
2023-05-31 04:03:54 +0000

    correction made to 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,7 +10,7 @@ class Square(object):
             size (int): size of the square
             Return: None
         """
-        if not isinstance(size, (int, float)):
+        if typeof(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
