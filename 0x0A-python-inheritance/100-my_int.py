3c2c7afa5111d9a6c9e038076533ea825d260700
Daniel Kwasi Dzrekey <dkdzrekey001@gmail.com>
2023-06-13 03:18:37 +0000

    a class MyInt that inherits from int

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""A class that Invert the meaning to the equal ==,
+    and not equal != sign
+"""
+
+
+class MyInt(int):
+    """Inherits form int"""
+    def __init__(self, integer):
+        """Initialize instance attributes"""
+        self.integer = integer
+
+    def __eq__(self, other):
+        """Invert the equal sign"""
+        return (self.integer != other)
+
+    def __nq__(self, other):
+        """Invert the not equal sign"""
+        return (self.integer == other)
