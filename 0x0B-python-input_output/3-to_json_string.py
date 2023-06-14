a421f1889a9ece74666ab9b041905671091b397a
Daniel Kwasi Dzrekey <dkdzrekey001@gmail.com>
2023-06-14 00:35:52 +0000

    imported json

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -2,6 +2,9 @@
 """returns the JSON representation of an object"""
 
 
+import json
+
+
 def to_json_string(my_obj):
     """return the JSON representation if an object"""
     return json.dumps(my_obj)
