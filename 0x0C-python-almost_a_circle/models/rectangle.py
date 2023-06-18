de0f752c836a77a4a3fd8bcd844af7ef0d3c30af
Daniel Kwasi Dzrekey <dkdzrekey001@gmail.com>
2023-06-18 01:12:42 +0000

    Updated the class Rectangle by adding validation of all setter methods and instantiation (id excluded):

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -13,6 +13,11 @@ class Rectangle(Base):
             x (int): x coordinate of the rectangle
             y (int): y coordinate of the rectangle
             id (id): id of the rectangle
+        Raises:
+            TypeError: If either of width or height is not an int.
+            ValueError: If either of width or height <= 0.
+            TypeError: If either of x or y is not an int.
+            ValueError: If either of x or y < 0.
         """
         super().__init__(id)
         self.width = width
@@ -27,6 +32,10 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
+        if value is not int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -36,6 +45,10 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
+        if value is not int:
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -45,6 +58,10 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
+        if value is not int:
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -54,4 +71,8 @@ class Rectangle(Base):
 
     @width.setter
     def y(self, value):
+        if value is not int:
+            raise TypeError("y must be an integer")
+        elif value <= 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
