d68eb9735470dadc45810dd6f096cb298888eb43
Daniel Kwasi Dzrekey <dkdzrekey001@gmail.com>
2023-06-20 00:10:02 +0000

    save_to_file() edited

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -32,10 +32,9 @@ class Base:
         Returns:
             json represation
         """
-        if (list_dictionaries is not None or len(list_dictionaries) == 0):
+        if list_dictionaries is not None or list_dictionaries == []:
             return "[]"
-        else:
-            return json.dumps(list_dictionaries)
+        return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
