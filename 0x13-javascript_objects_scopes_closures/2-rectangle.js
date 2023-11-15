95c43c763d64c12576e8c209d007e5a59937d3d3
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2023-11-15 12:20:16 +0000

    corrected semistandard errors

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -6,7 +6,7 @@
 
 class Rectangle {
   constructor (w = 0, h = 0) {
-    if (typeof(w) === 'number' && typeof(h) === 'number' &&  w > 0 && h > 0){
+    if (typeof(w) === 'number' && typeof(h) === 'number' &&  w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
