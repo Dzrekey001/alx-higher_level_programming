74857ef8e1c34f2c14883d12f77ae4b82777c021
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2023-11-18 16:03:24 +0000

    corrected query

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -12,7 +12,10 @@ if __name__ == "__main__":
             host="localhost", port=3306, user=argv[1],
             passwd=argv[2], db=argv[3])
     cursor = connection.cursor()
-    query = "SELECT * FROM cities ORDER BY cities.id"
+    query = "SELECT cities.id, cities.name, states.name \
+            FROM cities INNER JOIN states \
+            ON cities.state_id = states.id \
+            ORDER BY cities.id ASC"
 
     cursor.execute(query)
     selected_cities = cursor.fetchall()
